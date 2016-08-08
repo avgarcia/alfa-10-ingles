9b6e62aa161acbc81205ac29994aa9ad25f00d18
Antonio Vidal <lalin1982@gmail.com>
2016-08-08 21:53:02 +0200

    Configure Sass compiler

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,18 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('default', function() {
-    // place code for your default task here
+gulp.task('default', function () {
+});
+
+gulp.task('watch', function () {
+    gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
+gulp.task('sass', function () {
+    return gulp.src('./sass/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./css'));
 });
